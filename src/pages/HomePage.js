2d267165ec8a63ef125c650446a0cb7c3de0881a
route-2 <ruthurao@gmail.com>
2022-07-18 02:42:31 +0530

    push

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -46,11 +46,19 @@ getdata();
     <div class=" relative  flex flex-col md:flex-row md:space-x-4 md:space-y-4  h-fit">
      <div className='flex m-6 text-white flex-col w-[85%] md:w-[40%] bg-grey-100'>
 
-<div className='flex w-[100%] h-[90%] mt-5 justify-center md:mt-18 md:w-[100%] bg-gray-700 items-center rounded-lg  bg-opacity-30 backdrop-blur-xl  drop-shadow-lg text-white flex-col '>
-        <div className=' w-[80%] mt-8 rounded-lg   '>
-<img src={`https://ipfs.io/ipfs/${metadata}/image`} alt="" className='p-4'/>
+<div className='flex w-[100%] h-[90%] ml-10 mt-5 justify-center md:mt-18 md:w-[100%] bg-gray-700 items-center rounded-lg  bg-opacity-30 backdrop-blur-xl  drop-shadow-lg text-white flex-col '>
 
-        </div>
+       
+        <div class="mr-8 w-[70%] z-10 ">
+  <div class="grid gap-8 items-center justify-center">
+    <div class="relative group ">
+      <div class="absolute  -inset-0.5 bg-gradient-to-r from-indigo-600 to-purple-300 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+     
+     
+      <img src={`https://ipfs.io/ipfs/${metadata}/image`} alt="" className=' relative  bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600 p-4'/>
+    </div>
+  </div>
+</div>
         <div className='items-center mt-6 p-4 '>
 <h1 className='font-bold text-center '> {name} </h1>
 <h1 className='text-center font-caustic text-xl'> {description} </h1>
@@ -58,27 +66,41 @@ getdata();
 
 
         </div>
-        <div className='items-center hidden md:block mt-4 mb-10'>
-        <button class="bg-red-300  mt-4 hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-full inline-flex items-center">
+       
+<div class="mr-8 items-center hidden md:block mt-4 mb-10 z-10">
+  <div class="grid gap-8 items-start justify-center">
+    <div class="relative group">
+      <div class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+      <button class="   relative text-white mt-2 mb-2 px-7 py-3 bg-indigo-300 rounded-full leading-none flex items-center divide-x divide-gray-600 inline-flex items-center">
   <img src='https://svgshare.com/i/jBg.svg' alt='' className='mr-3'/>
   <a href='https://opensea.io/collection/shardsofeth'> 
   <span className='mr-2  '> <strong> About </strong> </span> </a>
 </button>
+    </div>
+  </div>
 </div>
-<div className=' md:hidden mb-4'>
- <button class="bg-red-300 mt-4 md:hidden hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded-full inline-flex items-center">
+      
+      <div class="mr-8 items-center md:hidden  mt-4 mb-10 z-10">
+  <div class="grid gap-8 items-start justify-center">
+    <div class="relative group">
+      <div class="absolute -inset-0.5 bg-gradient-to-r from-blue-600 to-purple-600 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+      <button class="   relative text-white mt-2 mb-2 px-7 py-3 bg-indigo-300 rounded-full leading-none flex items-center divide-x divide-gray-600 inline-flex items-center">
   <img src='https://svgshare.com/i/jBg.svg' alt='' className='mr-3'/>
   <a href='https://opensea.io/collection/shardsofeth'> 
-  <span className='mr-2 '> <strong> View on Opensea </strong> </span> </a>
+  <span className='mr-2  '> <strong> View On Opensea </strong> </span> </a>
 </button>
-        </div>
+    </div>
+  </div>
+</div>
+
+
        </div>
 
 
 
 <button class> </button>
      </div>
-     <div className='flex  text-white items-center justify-center  flex-col w-[100%] '>
+     <div className='flex ml-4 text-white items-center justify-center  flex-col w-[100%] '>
     
        <div className='flex w-[93%]  md:w-[90%] bg-gray-700 rounded-lg  bg-opacity-30 backdrop-blur-xl  drop-shadow-lg m-6 text-white flex-row '>
         <div className='bg-gray-400  rounded-lg  bg-opacity-30 backdrop-blur-xl  drop-shadow-lg m-6 text-white flex-row'>
