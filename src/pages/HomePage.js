661845ec79f450d00ed28e08e97af05fb7c12583
route-2 <ruthurao@gmail.com>
2022-07-19 01:36:04 +0530

    push

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -55,9 +55,9 @@ getdata();
   <div class="grid gap-8 items-center justify-center">
     <div class="relative group ">
       <div class="absolute  -inset-0.5 bg-gradient-to-r from-indigo-600 to-purple-300 mt-[20%] rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-2000 group-hover:duration-300 md:mt-[50%] animate-tilt"></div>
-     
-     
-      <img src={`https://ipfs.io/ipfs/${metadata}/image`} alt="" className=' relative mt-[20%] md:mt-[10%]  bg-black rounded-lg leading-none flex items-center '/>
+      {/* {`https://ipfs.io/ipfs/${metadata}/image`} */}
+  
+      <img src='https://svgshare.com/i/jDA.svg' alt="" className=' relative mt-[20%] md:mt-[10%]  bg-black rounded-lg leading-none flex items-center '/>
     </div>
   </div>
 </div>
