e1bd2f017560901e9a74005c1850e874a45c6970
route-2 <ruthurao@gmail.com>
2022-07-18 21:57:53 +0530

    push

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -39,29 +39,29 @@ getdata();
    
     <div class="bg-black min-h-screen flex ">
   <div class="relative w-screen h-screen">
-    <div className='md:fixed z-10 flex flex-row md:w-screen'>
+    <div className='md:absolute z-10 flex flex-row md:w-screen'>
       <NavBar/>
     </div>
  
     <div class="absolute top-0 -left-8  w-[40%] h-[80%] bg-gradient-to-r from-blue-500 to-purple-500 rounded-full mix-blend-lighten opacity-50 filter blur-3xl "></div>
     <div class="absolute top-0 -bottom-[50%] left-[50%] w-[40%] h-[40%] bg-gradient-to-r from-pink-400 to-purple-500  rounded-full mix-blend-lighten opacity-50 filter blur-3xl"></div>
-    <div class="absolute left-[70%] top-[70%]  w-[30%] h-[30%] bg-gradient-to-r from-red-300 to-yellow-50 rounded-full mix-blend-lighten filter opacity-50 blur-[100px]  "></div>
+    <div class="absolute md:left-[70%] left-[50%] top-[90%] md:top-[70%]  md:w-[30%] md:h-[30%] w-[40%] h-[40%] bg-gradient-to-r from-red-300 to-yellow-50 rounded-full mix-blend-lighten filter opacity-50 blur-[100px]  "></div>
     <div class=" relative  flex flex-col md:flex-row md:space-x-4 md:space-y-4  h-fit">
-     <div className='flex text-white m-6 md:m-0 flex-col w-fit md:w-[50%] bg-grey-100'>
+     <div className='flex text-white md:h-screen m-6 md:m-0 flex-col w-fit md:w-[50%] bg-grey-100'>
 
-     <div className='flex w-[100%] md:h-screen justify-center md:mt-18 md:w-[100%] bg-white items-center rounded-xl md:rounded-none  bg-opacity-10 backdrop-blur-xl  drop-shadow-lg text-white flex-col '>
+     <div className='flex w-[100%] md:h-[100vmax] justify-center md:mt-18 md:w-[100%] bg-white items-center rounded-xl md:rounded-none  bg-opacity-10 backdrop-blur-xl  drop-shadow-lg text-white flex-col '>
        
         <div class=" w-[70%] z-10 ">
   <div class="grid gap-8 items-center justify-center">
     <div class="relative group ">
-      <div class="absolute  -inset-0.5 bg-gradient-to-r from-indigo-600 to-purple-300 mt-[20%] rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-2000 group-hover:duration-300 animate-tilt"></div>
+      <div class="absolute  -inset-0.5 bg-gradient-to-r from-indigo-600 to-purple-300 mt-[20%] rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-2000 group-hover:duration-300 md:mt-[50%] animate-tilt"></div>
      
      
-      <img src={`https://ipfs.io/ipfs/${metadata}/image`} alt="" className=' relative mt-[20%]  bg-black rounded-lg leading-none flex items-center '/>
+      <img src={`https://ipfs.io/ipfs/${metadata}/image`} alt="" className=' relative mt-[20%] md:mt-[50%]  bg-black rounded-lg leading-none flex items-center '/>
     </div>
   </div>
 </div>
-        <div className='items-center mt-6 p-4 '>
+        <div className='items-center  p-4 '>
 <h1 className='font-bold text-center font-causten '> {name} </h1>
 <h1 className='text-center font-causten text-xl'> {description} </h1>
 
@@ -69,7 +69,7 @@ getdata();
 
         </div>
        
-<div class="  items-center hidden md:block mt-4 mb-10 z-10">
+<div class="  items-center hidden md:block  mb-10 z-10">
   <div class="grid gap-8 items-start justify-center">
     <div class="relative group">
       <div class="absolute -inset-0.5 bg-gradient-to-r from-blue-500 to-purple-600 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"> </div>
@@ -102,9 +102,9 @@ getdata();
 
 
      </div>
-     <div className='flex text-white pt-4 items-center justify-center  flex-col w-[100%] '>
+     <div className='flex text-white items-center justify-center  flex-col w-[100%] '>
      
-     <div className='flex w-[93%] md:w-[80%] bg-black hover:shadow-lg hover:shadow-blue-500/50 rounded-3xl items-center  bg-opacity-50 backdrop-blur-xl  drop-shadow-lg m-2 text-white flex-row '>
+     <div className='flex w-[93%] md:w-[80%] bg-black hover:shadow-lg hover:shadow-blue-500/50 rounded-3xl items-center  bg-opacity-50 backdrop-blur-xl  drop-shadow-lg m-2 text-white flex-row md:mt-[60px] '>
         <div className='bg-gray-400  rounded-2xl  bg-opacity-30 backdrop-blur-xl  drop-shadow-lg m-2 text-white flex-row'>
 <img src="https://svgshare.com/i/jAD.svg" alt="" className='p-4'/>
 
