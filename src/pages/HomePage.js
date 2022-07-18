8dc0ea26f8664874f82d4c19ebdf55a412a0b932
route-2 <ruthurao@gmail.com>
2022-07-18 20:31:50 +0530

    push

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -44,12 +44,12 @@ getdata();
     </div>
  
     <div class="absolute top-0 -left-8  w-[40%] h-[80%] bg-gradient-to-r from-blue-500 to-purple-500 rounded-full mix-blend-lighten opacity-50 filter blur-3xl "></div>
-    <div class="absolute top-0 -bottom-10 left-[40%] w-[50%] h-[40%] bg-gradient-to-r from-pink-500 to-violet-500 rounded-full mix-blend-lighten opacity-50 filter blur-3xl"></div>
-    <div class="absolute left-[40%] top-[50%]  w-[60%] h-[50%] bg-gradient-to-r from-red-300 to-yellow-100 rounded-full mix-blend-lighten filter opacity-50 blur-[100px]  "></div>
+    <div class="absolute top-0 -bottom-[50%] left-[50%] w-[40%] h-[40%] bg-gradient-to-r from-pink-400 to-purple-500  rounded-full mix-blend-lighten opacity-50 filter blur-3xl"></div>
+    <div class="absolute left-[50%] top-[70%]  w-[40%] h-[50%] bg-gradient-to-r from-red-300 to-yellow-50 rounded-full mix-blend-lighten filter opacity-50 blur-[100px]  "></div>
     <div class=" relative  flex flex-col md:flex-row md:space-x-4 md:space-y-4  h-fit">
      <div className='flex text-white m-6 md:m-0 flex-col w-fit md:w-[50%] bg-grey-100'>
 
-<div className='flex w-[100%] md:h-screen justify-center md:mt-18 md:w-[100%] bg-white items-center rounded-xl md:rounded-none  bg-opacity-10 backdrop-blur-xl  drop-shadow-lg text-white flex-col '>
+<div className='flex w-[100%] md:h-screen justify-center md:mt-18 md:w-[100%] bg-white items-center rounded-xl md:rounded-none h-screen  bg-opacity-10 backdrop-blur-xl  drop-shadow-lg text-white flex-col '>
 
        
         <div class=" w-[70%] z-10 ">
@@ -63,8 +63,8 @@ getdata();
   </div>
 </div>
         <div className='items-center mt-6 p-4 '>
-<h1 className='font-bold text-center '> {name} </h1>
-<h1 className='text-center font-caustic text-xl'> {description} </h1>
+<h1 className='font-bold text-center font-causten '> {name} </h1>
+<h1 className='text-center font-causten text-xl'> {description} </h1>
 
 
 
@@ -77,7 +77,7 @@ getdata();
       <button class="   relative text-white mt-2 mb-2 px-7 py-3 bg-indigo-800 rounded-full leading-none flex items-center  inline-flex items-center">
   <img src='https://svgshare.com/i/jBg.svg' alt='' className='mr-3'/>
   <a href='https://opensea.io/collection/shardsofeth'> 
-  <span className='mr-2  '> <strong> About </strong> </span> </a>
+  <span className='mr-2 font-causten  '> <strong> About </strong> </span> </a>
 </button>
     </div>
   </div>
@@ -90,7 +90,7 @@ getdata();
       <button class="   relative text-white mt-2 mb-2 px-7 py-3 bg-indigo-800 rounded-full leading-none flex items-center  inline-flex items-center">
   <img src='https://svgshare.com/i/jB7.svg' alt='' className='mr-3'/>
   <a href='https://opensea.io/collection/shardsofeth'> 
-  <span className='mr-2  '> <strong> View On Opensea </strong> </span> </a>
+  <span className='mr-2 font-causten '> <strong> View On Opensea </strong> </span> </a>
 </button>
     </div>
   </div>
@@ -115,7 +115,7 @@ getdata();
         <a href='https://opensea.io/collection/shardsofeth'> 
 <h1 className='font-bold text-lg md:text-xl '> View on IPFS / ARWEAVE </h1>    </a>
 
-<h3 className=' font-caustic hidden md:block'> See the unlockables for this NFT on the link provided </h3>
+<h3 className=' font-causten hidden md:block'> See the unlockables for this NFT on the link provided </h3>
         </div>
        </div>
      
@@ -127,7 +127,7 @@ getdata();
         <div className='items-center '>
 <h1 className='font-bold text-lg md:text-xl'> View AR experience </h1>
 
-<h3 className='font-caustic hidden md:block'> See how your NFT looks in augmented reality </h3>
+<h3 className='font-causten hidden md:block'> See how your NFT looks in augmented reality </h3>
         </div>
        </div>
        <div className='flex w-[93%] hover:shadow-lg hover:shadow-blue-500/50 md:w-[80%] bg-black rounded-3xl items-center  bg-opacity-50 backdrop-blur-xl  drop-shadow-lg m-2 text-white flex-row '>
@@ -138,7 +138,7 @@ getdata();
         <div className='items-center '>
 <h1 className='font-bold text-lg md:text-xl'> Download 3D Model </h1>
 
-<h3 className=' font-caustic hidden md:block'> Instantly get access to the blend file</h3>
+<h3 className=' font-causten hidden md:block'> Instantly get access to the blend file</h3>
         </div>
        </div>
        <div className='flex hover:shadow-lg hover:shadow-blue-500/50 w-[93%] md:w-[80%] bg-black rounded-3xl items-center  bg-opacity-50 backdrop-blur-xl  drop-shadow-lg m-2 text-white flex-row '>
@@ -146,11 +146,11 @@ getdata();
 <img src="https://svgshare.com/i/jAE.svg" alt="" className='p-4'/>
 
         </div>
-        <div className='items-center text-lg md:text-xl'>
+        <div className='items-center '>
         <a href = 'https://frames.dehidden.com/'> 
-<h1 className='font-bold'> Join the community </h1></a>
+<h1 className='text-lg md:text-xl font-bold'> Join the community </h1></a>
 
-<h3 className=' font-caustic hidden md:block'> Customise your NFTs specification </h3>
+<h3 className=' font-causten hidden md:block'> Customise your NFTs specification </h3>
         </div>
        </div>
       
