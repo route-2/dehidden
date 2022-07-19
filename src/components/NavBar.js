65f35b9b7ae4d93ecd36f04ffc1dc4f60040d9f8
route-2 <ruthurao@gmail.com>
2022-07-19 13:04:06 +0530

    push

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -63,7 +63,7 @@ const NavBar = () => {
   <div class="grid gap-8 items-start justify-center">
     <div class="relative group">
       <div class="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-full blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-      <button onClick={onClick} className="relative text-white px-7 ml-2 py-4 bg-black rounded-full leading-none flex items-center divide-x divide-gray-600" >
+      <button onClick={onClick} className="relative text-white px-6 ml-2 py-2 bg-black rounded-full leading-none flex items-center divide-x divide-gray-600" >
         
        Share
       </button>
